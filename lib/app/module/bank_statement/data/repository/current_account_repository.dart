3126ed865d5cd67a9f6a14132da6e01831688f29
NataBandeira <nata.wsd@gmail.com>
2022-05-25 17:14:05 -0300

    create data layer

---
+++ lib/app/module/bank_statement/data/repository/current_account_repository.dart
@@ -0,0 +1,9 @@
+import '../../domain/domain.dart';
+
+class CurrentAccountRepository extends BankStatementRepository {
+  @override
+  Future<BankStatement> getAccountTransactions(String token) {
+    // TODO: implement getAccountTransactions
+    throw UnimplementedError();
+  }
+}
